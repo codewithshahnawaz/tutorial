b7b5a8c7bcb40c918d3823fb6424792ec0970b4d
codewithshahnawaz <45561961+codewithshahnawaz@users.noreply.github.com>
2018-12-04 12:52:00 +0530

    second commit

---
+++ hangman.py
@@ -0,0 +1,21 @@
+from random import *
+names = ['hospital', 'linux', 'android', 'fedora', 'word', 'helicopter']
+shuffle(names)
+display = []
+display.extend(list(names[0]))
+for i in range(len(display)):
+    display[i] = "_"
+print(' '.join(display))
+print()
+count = 0
+while count < len(list(names[0])):
+	guess = input('Please guess a %d letter: '% len(list(names[0])))
+    guess = guess.lower()
+    print(count)
+    for i in range(len(list(names[0]))):
+        if list(names[0])[i] == guess:
+            display[i] = guess
+            count += 1
+    print(' '.join(display))
+    print()
+print("Well done,you guessed the word")
